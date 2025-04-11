293df0f7dd0d6f6e9400d92b718bc02103255b97
chordfrenzy <141478076+chordfrenzy@users.noreply.github.com>
2025-04-11 17:24:55 +0700

    Create block-ip.js

---
+++ block-ip.js
@@ -0,0 +1,13 @@
+const blockedIPs = ["211.24.168.146"];
+
+// Ambil IP publik pengunjung
+fetch("https://ipapi.co/json")
+  .then((res) => res.json())
+  .then((data) => {
+    const userIP = data.ip;
+    if (blockedIPs.includes(userIP)) {
+      // Aksi blokir - bisa redirect, sembunyikan halaman, atau tampilkan pesan
+      window.location.href = "https://google.com";
+    }
+  })
+  .catch((err) => console.error("Gagal cek IP", err));
